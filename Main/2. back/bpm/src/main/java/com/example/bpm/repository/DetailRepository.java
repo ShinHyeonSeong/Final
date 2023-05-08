8a0138e338d55a71c8a09b8b82621c37888b82ce
DESKTOP-EI92TPF\park <parkjonghyeok2000@gmail.com>
2023-05-08 14:37:54 +0900

    0508 프로젝트 병합 충돌 해결

--- Main/2. back/bpm/src/main/java/com/example/bpm/repository/DetailRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.bpm.repository;
-
-import com.example.bpm.entity.DetailEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DetailRepository extends JpaRepository<DetailEntity, Long> {
-}
--- Main/2. back/bpm/src/main/java/com/example/bpm/repository/WorkRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.bpm.repository;
-
-import com.example.bpm.entity.WorkEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WorkRepository  extends JpaRepository<WorkEntity, Long> {
-}
