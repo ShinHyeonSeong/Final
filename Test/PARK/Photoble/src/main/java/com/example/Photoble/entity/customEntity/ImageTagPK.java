5380ae940dd74e295d198212627269cdafbf1936
DESKTOP-HUDI05M\shin0 <shin0586@naver.com>
2023-05-08 14:35:08 +0900

    신현성 : 프로젝트 분할(2)

--- Test/PARK/Photoble/src/main/java/com/example/Photoble/entity/customEntity/ImageTagPK.java
+++
@@ -1,9 +0,0 @@
-package com.example.Photoble.entity.customEntity;
-
-
-import java.io.Serializable;
-
-public class ImageTagPK implements Serializable {
-    private String image;
-    private String tag;
-}
