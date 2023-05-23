677e18080d31388ae0fc2d32f6c54001842fde0a
marangY <jwkid9124@gmail.com>
2023-05-24 01:14:56 +0900

    add contextmenu
    - add custom contextmenu
    - add block delete menu

--- Main/demo/src/main/resources/static/js/documentWrite.js
+++ Main/demo/src/main/resources/static/js/documentWrite.js
@@ -118,3 +118,115 @@ $(document).keydown(function(e) {
 });
 
 /* context 메뉴 */
+
+//이벤트
+
+function handleCreateContextMenu(event) {
+    event.preventDefault();
+
+    const ctxMenuFind = document.getElementById('block_context_menu');
+    if (ctxMenuFind != null) {
+        ctxMenuFind.remove();
+    }
+
+    const ctxMenu = createContextMenu(event.target);
+
+    ctxMenu.style.display = 'block';
+
+    ctxMenu.style.top = event.pageY+'px';
+    ctxMenu.style.left = event.pageX+'px';
+
+    document.body.appendChild(ctxMenu);
+
+    addMenuEvent(event.target);
+}
+
+function handleClearContextMenu() {
+    const ctxMenu = document.getElementById('block_context_menu');
+
+    if (ctxMenu == null) {
+        return;
+    }
+    else {
+        // 컨텍스트 메뉴 삭제
+        ctxMenu.remove();
+    }
+
+}
+
+// 메뉴 공간
+function createContextMenu(target) {
+
+    // Context Menu Element 생성
+    const ctxMenuId = 'block_context_menu';
+    const ctxMenu = document.createElement('div');
+
+    // Context Menu Element 옵션 설정
+    ctxMenu.id = ctxMenuId;
+    ctxMenu.className = 'custom-context-menu';
+
+    // Menu ul 생성
+    const menuListId = 'context-menu-list';
+    const menuList = document.createElement('div');
+
+    // Menu ul 옵션 설정
+    menuList.id = menuListId;
+    menuList.className = 'custom-context-menu-list';
+
+    /////////////////////////////////////////////////////////////////
+
+    // 삭제 메뉴 추가
+    const deleteMenuItem = createDeleteMenu();
+    menuList.appendChild(deleteMenuItem);
+
+
+    /////////////////////////////////////////////////////////////////
+
+    ctxMenu.appendChild(menuList);
+
+    return ctxMenu;
+}
+
+function createDeleteMenu() {
+    const deleteMenuId = 'delete-menu';
+    const deleteMenu = document.createElement('li');
+
+    deleteMenu.id = deleteMenuId;
+    deleteMenu.className = 'menu-item';
+    deleteMenu.innerText = '블럭 삭제';
+
+    return deleteMenu;
+}
+
+/* 블럭 기능 구현 */
+function addMenuEvent(target){
+
+    // 삭제 메뉴 기능
+    $(function(){
+        $("#delete-menu").on('click' ,function(e){
+            if(target.className == 'move_block'){
+                target.remove();
+            }
+            else if (target.className == 'block' || target.className == 'move ui-sortable-handle' || target.className == 'move'){
+                target.parentNode.remove();
+            }
+            else{
+                target.parentNode.parentNode.remove();
+            }
+        });
+    });
+}
+
+
+// 이벤트 등록
+function addContextMenuEvent(){
+    $(function(){
+        $(".move_block").on('contextmenu' ,function(e){
+            handleCreateContextMenu(e);
+        });
+    });
+
+    document.addEventListener('click', handleClearContextMenu, false);
+}
+
+addContextMenuEvent();
